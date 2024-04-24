057d548dfeda04d0898c12362a0398f8993d8ba4
Dawdax <davidaguilar5034@gmail.com>
2024-04-24 06:13:25 -0600

    Se agregarón los estilos css, template y se probó handlebars

---
+++ controllers/homeController.js
@@ -0,0 +1,8 @@
+exports.mostrarTrabajo = (req, res) =>{
+    res.render('home', {
+        nombrePagina: 'devJobs',
+        tagline: 'Encuentra y pública trabajos para desarrolladores web',
+        barra: true,
+        boton: true
+    })
+}
